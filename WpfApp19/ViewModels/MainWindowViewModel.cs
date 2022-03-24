070b969e7616f24166461a4df9f3d7a2e4d92b4d
Татьяна Буянова <tanyasilagina@gmail.com>
2022-03-24 13:52:51 +0300

    Добавьте файлы проекта.

---
+++ WpfApp19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfApp19.Models;
+
+namespace WpfApp19.ViewModels
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string PropertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        private int number1;
+        public int Number1
+        {
+            get=> number1;
+            set
+            {
+                number1 = value;
+                OnPropertyChanged();
+            }
+        }
+        private int number2;
+        public int Number2
+        {
+            get => number2;
+            set
+            {
+                number2 = value;
+                OnPropertyChanged();
+            }
+        }
+        private int number3;
+        public int Number3
+        {
+            get => number3;
+            set
+            {
+                number3 = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Number3 = Ariph.Add(Number1,Number2);
+        }
+        private bool CanAddCommandExecuted (object p)
+        {
+            if (Number1 != 0 || Number2 != 0)
+                return true;
+            else
+                return
+                    false;
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+}
